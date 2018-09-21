6c1c138bb3e3880732dc8879f9c5f13f547ca87b
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-21 18:48:21 +0300

    three tests

--- learntest/Test.cs
+++ learntest/Test.cs
@@ -1,13 +1,39 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 namespace learntest
 {
-    [TestFixture()]
-    public class Test
+    [TestFixture] 
+    public class Class1 
     {
-        [Test()]
-        public void TestCase()
+        [Test] 
+        public void go_to_home_page() 
         {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("https://www.olx.ua/rabota/stroitelstvo/dnepr/");
+            IWebElement button = browser.FindElement(By.CssSelector("#headerLogo"));
+            button.Click();
+            browser.Close();
+
+        }
+        [Test]
+        public void submit_search()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("https://www.olx.ua");
+            IWebElement button = browser.FindElement(By.CssSelector("#submit-searchmain"));
+            button.Click();
+            browser.Close();
+        }
+        [Test]
+        public void all_in_deti()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("https://www.olx.ua");
+            IWebElement button = browser.FindElement(By.CssSelector("#bottom36 > div"));
+            button.Click();
+            browser.Close();
         }
     }
 }
